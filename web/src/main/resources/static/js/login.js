bc1dfba3e23e1ce5511ff319e4d4fb6d69151a8c
erafaelmanuel <erafaelmanuel@gmail.com>
2017-10-27 18:11:43 +0800

    Update the design of the login page.

--- web/src/main/resources/static/js/login.js
+++ web/src/main/resources/static/js/login.js
@@ -1,8 +1,7 @@
 (function() {
-    var username = document.getElementById("username");
-    var password = document.getElementById("password");
-
     document.getElementById("form-login").onsubmit = function(){
+        var username = document.getElementById("username");
+        var password = document.getElementById("password");
         if(!validate(username, password)) return false;
     };
 
@@ -14,22 +13,42 @@
             })
             changeState(e);
         }
+        document.querySelector("#username").addEventListener("focusin", function() {
+            document.querySelector("#username + label").style.color="#16a085";
+            document.querySelector("#username + label").style.opacity=1;
+            document.querySelector("#username + label").innerHTML="Username";
+        });
+
+        document.querySelector("#password").addEventListener("focusin", function() {
+            document.querySelector("#password + label").style.color="#16a085";
+            document.querySelector("#password + label").style.opacity=1;
+            document.querySelector("#password + label").innerHTML="Password";
+        });
+
+        document.querySelector("#username").addEventListener("focusout", function() {
+            document.querySelector("#username + label").style.color="#000";
+            document.querySelector("#username + label").style.opacity=0.6;
+        });
+        document.querySelector("#password").addEventListener("focusout", function() {
+            document.querySelector("#password + label").style.color="#000";
+            document.querySelector("#password + label").style.opacity=0.6;
+        });
+
+        
     })();
 
     function validate(username, password) {
         var isValid = true;
 
         if(username.value.trim() === "") {
-            username.style.border = "1px solid #c0392b";
+            document.querySelector("#username + label").style.color="#c0392b";
+            document.querySelector("#username + label").innerHTML="Please enter a username";
             isValid = false;
-        } else {
-            username.style.border = "1px solid #bdc3c7";
         }
         if(password.value.trim() === "") {
-            password.style.border = "1px solid #c0392b";
+            document.querySelector("#password + label").style.color="#c0392b";
+            document.querySelector("#password + label").innerHTML="Please enter a password";
             isValid = false;
-        } else {
-            password.style.border = "1px solid #bdc3c7";
         }
         return isValid;
     }
--- web/src/main/resources/static/js/obj/User.js
+++
@@ -1,4 +0,0 @@
-function User(username, password) {
-    this.username=username;
-    this.password=password;
-}
