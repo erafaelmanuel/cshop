ddd0eaddf279265c10c759fd42a07bae64d644ff
erafaelmanuel <erafaelmanuel@gmail.com>
2017-10-29 21:16:29 +0800

    Create a customer model.

---
+++ model/src/main/java/io/ermdev/ecloth/model/entity/Customer.java
@@ -0,0 +1,12 @@
+package io.ermdev.ecloth.model.entity;
+
+public class Customer {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String address;
+    private String contact;
+    private String email;
+}
