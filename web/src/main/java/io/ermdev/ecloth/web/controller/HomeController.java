d847d0e06ec64a688594f911c2bee19eacfdb80b
erafaelmanuel <erafaelmanuel@gmail.com>
2017-11-01 14:20:00 +0800

    Create a session attribute items and replace the dummy items of shopping cart.

--- web/src/main/java/io/ermdev/ecloth/web/controller/HomeController.java
+++ web/src/main/java/io/ermdev/ecloth/web/controller/HomeController.java
@@ -1,15 +1,38 @@
 package io.ermdev.ecloth.web.controller;
 
+import io.ermdev.ecloth.data.exception.EntityNotFoundException;
+import io.ermdev.ecloth.data.service.ItemService;
+import io.ermdev.ecloth.model.entity.Item;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@SessionAttributes({"items"})
 @RequestMapping
 public class HomeController {
 
+    @Autowired
+    private ItemService itemService;
+
     @GetMapping
-    public String showHomePage() {
+    public String showHomePage(ModelMap modelMap) {
+
+
+        List<Item> items = null;
+        try {
+            items = itemService.findAll();
+        } catch (EntityNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        modelMap.addAttribute("items", items);
         return "home";
     }
 }
