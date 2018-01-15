1cd0814b577f97f3c5700891adcd07fbd051cb6f
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-15 23:23:04 +0800

    Replace the simple to model mapper.

--- commons/src/main/java/io/ermdev/cshop/commons/bean/MapperBean.java
+++ commons/src/main/java/io/ermdev/cshop/commons/bean/MapperBean.java
@@ -1,5 +1,6 @@
 package io.ermdev.cshop.commons.bean;
 
+import io.ermdev.mapfierj.ModelMapper;
 import io.ermdev.mapfierj.SimpleMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +11,10 @@ public class MapperBean {
     @Bean
     public SimpleMapper simpleMapper() {
         return new SimpleMapper();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
