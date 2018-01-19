7e55c5158d7ca907fb8a57e2ae1aa81dffa8379a
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-19 16:46:14 +0800

    Create a register mail and refactor the register listener.

--- business/src/main/java/io/ermdev/cshop/business/register/RegisterListener.java
+++ business/src/main/java/io/ermdev/cshop/business/register/RegisterListener.java
@@ -1,35 +1,28 @@
 package io.ermdev.cshop.business.register;
 
-import io.ermdev.cshop.business.util.MailConstructor;
-import io.ermdev.cshop.data.entity.VerificationToken;
 import io.ermdev.cshop.data.service.VerificationTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.MessageSource;
 import org.springframework.mail.MailParseException;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.stereotype.Component;
 
 import javax.mail.MessagingException;
 import java.io.UnsupportedEncodingException;
-import java.util.Locale;
 
 @Component
 public class RegisterListener implements ApplicationListener<RegisterEvent> {
 
     private VerificationTokenService verificationTokenService;
     private JavaMailSender mailSender;
-    private MessageSource messageSource;
-    private MailConstructor mailConstructor;
+    private RegisterMail registerMail;
 
     @Autowired
     public RegisterListener(VerificationTokenService verificationTokenService, JavaMailSender mailSender,
-                                MessageSource messageSource, MailConstructor mailConstructor) {
+                            RegisterMail registerMail) {
         this.verificationTokenService = verificationTokenService;
         this.mailSender = mailSender;
-        this.messageSource = messageSource;
-        this.mailConstructor = mailConstructor;
+        this.registerMail = registerMail;
     }
 
     @Override
@@ -43,13 +36,8 @@ public class RegisterListener implements ApplicationListener<RegisterEvent> {
 
     private void confirmRegistration(RegisterEvent event) throws UnsupportedEncodingException, MessagingException {
         final RegisterSource source = (RegisterSource) event.getSource();
-        final VerificationToken verificationToken = source.getVerificationToken();
-        final String url = source.getUrl();
-        final Locale locale = source.getLocale();
 
-        verificationTokenService.add(verificationToken);
-
-        MimeMailMessage mailMessage = mailConstructor.constructVerificationMail(verificationToken, url, locale);
-        mailSender.send(mailMessage.getMimeMessage());
+        verificationTokenService.add(source.getVerificationToken());
+        mailSender.send(registerMail.constructVerificationMail(source).getMimeMessage());
     }
 }
