4361393ce355210f672e84ce0c1780805467a7be
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-20 18:58:35 +0800

    Update the catalog controller.

--- web/src/main/java/io/ermdev/cshop/web/controller/CatalogController.java
+++ web/src/main/java/io/ermdev/cshop/web/controller/CatalogController.java
@@ -1,12 +1,8 @@
 package io.ermdev.cshop.web.controller;
 
-import io.ermdev.cshop.data.exception.EntityNotFoundException;
-import io.ermdev.cshop.data.helper.CategoryHelper;
-import io.ermdev.cshop.data.helper.ItemHelper;
-import io.ermdev.cshop.data.service.CategoryService;
-import io.ermdev.cshop.data.service.ItemService;
-import io.ermdev.cshop.data.entity.Category;
 import io.ermdev.cshop.data.entity.Item;
+import io.ermdev.cshop.data.exception.EntityNotFoundException;
+import io.ermdev.cshop.data.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,77 +12,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 @SessionAttributes({"cartItems", "hasUser", "userName"})
 public class CatalogController {
 
-    private final ItemService itemService;
-    private final CategoryService categoryService;
+    private final long ITEM_PER_PAGE = 20;
 
-    private final CategoryHelper categoryHelper;
-    private final ItemHelper itemHelper;
+    private final ItemService itemService;
 
     @Autowired
-    public CatalogController(ItemService itemService, CategoryService categoryService, ItemHelper itemHelper,
-                             CategoryHelper categoryHelper) {
+    public CatalogController(ItemService itemService) {
         this.itemService = itemService;
-        this.categoryService = categoryService;
-        this.itemHelper = itemHelper;
-        this.categoryHelper = categoryHelper;
     }
 
     @GetMapping("catalog")
-    public String showCatalog(ModelMap modelMap,
-                       @RequestParam(required = false, value = "q") String query,
-                       @RequestParam(required = false, value = "c") Long categoryId,
-                       @RequestParam(required = false, value = "p") Long parentId,
-                       @RequestParam(required = false, value = "page") Integer page) {
+    public String showCatalog(ModelMap modelMap, @RequestParam(required = false, value = "page") Integer page) {
         try {
-            final List<Item> items = new ArrayList<>();
-            final List<Category> categories = new ArrayList<>();
-            long itemCount=0;
-            int pageCount=0;
 
-            if(query != null) {
-                items.addAll(itemService.findByName(query));
-                itemCount= (long) items.size();
-            } else if(categoryId != null) {
-                items.addAll(itemHelper.searchFromCategories(categoryHelper.startWith(categoryId)));
-                itemCount= (long) items.size();
-            } else {
-                itemCount= itemService.countAll();
-                pageCount = (int) (itemCount/20);
-                pageCount = pageCount * 20 == itemCount ? pageCount : pageCount+1;
-
-                if(page == null || page < 1 || page > pageCount)
-                    page=1;
-                if(page == 1)
-                    items.addAll(itemService.findAll(1, 20));
-                else
-                    items.addAll(itemService.findAll(((page-1) * 20) + 1, 20));
-            }
-            if(parentId != null) {
-                categories.addAll(categoryService.findByParent(parentId));
-            } else {
-                categories.add(categoryService.findById(1L));
-            }
-
-            if(page == null || page < 1 || page > pageCount)
-                page=1;
-
-            modelMap.addAttribute("items", items);
-            modelMap.addAttribute("itemCount", itemCount);
-            modelMap.addAttribute("pageCount", pageCount);
-            modelMap.addAttribute("category", "All");
-            modelMap.addAttribute("categories", categories);
-            modelMap.addAttribute("page", page);
-        } catch (EntityNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
         return "catalog";
     }
 
