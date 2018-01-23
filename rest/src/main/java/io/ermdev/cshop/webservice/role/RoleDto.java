e60b9300c69976bdc0e7dcf2dd8e361b84579176
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-23 22:20:21 +0800

    Create a Role dto

---
+++ rest/src/main/java/io/ermdev/cshop/webservice/role/RoleDto.java
@@ -0,0 +1,39 @@
+package io.ermdev.cshop.webservice.role;
+
+import io.ermdev.cshop.data.model.Link;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoleDto {
+
+    private Long id;
+    private String name;
+    private List<Link> links = new ArrayList<>();
+
+    public RoleDto() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Link> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<Link> links) {
+        this.links = links;
+    }
+}
