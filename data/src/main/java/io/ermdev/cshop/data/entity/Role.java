536146f0f2845e14285fd5547e0e4e14385baafc
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-23 20:08:36 +0800

    Update the role repository.

--- data/src/main/java/io/ermdev/cshop/data/entity/Role.java
+++ data/src/main/java/io/ermdev/cshop/data/entity/Role.java
@@ -7,10 +7,6 @@ public class Role {
 
     public Role() {}
 
-    public Role(String name) {
-        this.name = name;
-    }
-
     public Long getId() {
         return id;
     }
--- data/src/main/java/io/ermdev/cshop/data/repository/RoleRepository.java
+++ data/src/main/java/io/ermdev/cshop/data/repository/RoleRepository.java
@@ -1,11 +1,28 @@
 package io.ermdev.cshop.data.repository;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
+import io.ermdev.cshop.data.entity.Role;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface RoleRepository {
 
     @Insert("CREATE TABLE IF NOT EXISTS tbl_role(id BIGINT NOT NULL AUTO_INCREMENT, name VARCHAR(45), PRIMARY KEY(id))")
     void createTable();
+
+    @Select("SELECT * FROM tbl_role WHERE id=#{roleId}")
+    Role findById(@Param("roleId") Long roleId);
+
+    @Select("SELECT * FROM tbl_role")
+    List<Role> findAll();
+
+    @Insert("INSERT INTO tbl_role(id, name) VALUES(#{id}, #{name})")
+    void add(Role role);
+
+    @Update("UPDATE tbl_role SET name=#{name} WHERE id=#{id}")
+    void update(Role role);
+
+    @Delete("DELETE FROM tbl_role WHERE id=#{id}")
+    void delete(Role role);
 }
