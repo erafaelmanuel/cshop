6cdc2ef49a581b940831e308c4d778e08e666e85
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-24 12:07:28 +0800

    Create addRole and deleteRole method on UserRoleRepository

--- data/src/main/java/io/ermdev/cshop/data/repository/UserRoleRepository.java
+++ data/src/main/java/io/ermdev/cshop/data/repository/UserRoleRepository.java
@@ -23,4 +23,10 @@ public interface UserRoleRepository {
     @Select("SELECT * FROM tbl_role AS A LEFT JOIN tbl_user_role AS B ON A.id = B.roleId " +
             "WHERE B.userId=#{userId}")
     List<Role> findRolesByUserId(@Param("userId") Long userId);
+
+    @Insert("INSERT INTO tbl_user_role(userId, roleId) VALUES(#{userId, #{roleId})")
+    void addRole(@Param("userId") Long userId, @Param("roleId") Long roleId);
+
+    @Insert("DELETE FROM tbl_user_role WHERE userId=#{userId} AND roleId=#{roleId}")
+    void deleteRole(@Param("userId") Long userId, @Param("roleId") Long roleId);
 }
