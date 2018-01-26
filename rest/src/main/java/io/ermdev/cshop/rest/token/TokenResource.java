626ebcaee94df158049a31ca0ee760de2134066b
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-26 21:53:16 +0800

    Update the UserResource

--- rest/src/main/java/io/ermdev/cshop/rest/token/TokenResource.java
+++ rest/src/main/java/io/ermdev/cshop/rest/token/TokenResource.java
@@ -1,4 +1,46 @@
 package io.ermdev.cshop.rest.token;
 
+import io.ermdev.cshop.commons.Error;
+import io.ermdev.cshop.data.service.TokenService;
+import io.ermdev.cshop.exception.EntityException;
+import io.ermdev.mapfierj.SimpleMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+@Component
+@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+@Path("token")
 public class TokenResource {
+
+    private TokenService tokenService;
+    private SimpleMapper simpleMapper;
+
+    @Autowired
+    public TokenResource(TokenService tokenService, SimpleMapper simpleMapper) {
+        this.tokenService = tokenService;
+        this.simpleMapper = simpleMapper;
+    }
+
+    @GetMapping
+    @Path("{tokenId}")
+    public Response getById(@PathParam("tokenId") Long tokenId, @Context UriInfo uriInfo) {
+        try {
+            TokenDto tokenDto = simpleMapper.set(tokenService.findById(tokenId)).mapTo(TokenDto.class);
+            return null;
+        } catch (EntityException e) {
+            Error error = new Error(e.getMessage());
+            return Response.status(Response.Status.NOT_FOUND).entity(error).build();
+        }
+    }
 }
--- rest/src/main/java/io/ermdev/cshop/rest/user/UserResource.java
+++ rest/src/main/java/io/ermdev/cshop/rest/user/UserResource.java
@@ -1,9 +1,9 @@
 package io.ermdev.cshop.rest.user;
 
-import io.ermdev.cshop.data.entity.User;
-import io.ermdev.cshop.exception.EntityException;
 import io.ermdev.cshop.commons.Error;
+import io.ermdev.cshop.data.entity.User;
 import io.ermdev.cshop.data.service.UserService;
+import io.ermdev.cshop.exception.EntityException;
 import io.ermdev.mapfierj.SimpleMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -18,25 +18,25 @@ import java.util.List;
 @Component
 @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-@Path("user")
+@Path("users")
 public class UserResource {
 
     private UserService userService;
     private UserRoleResource userRoleResource;
-    private SimpleMapper mapper;
+    private SimpleMapper simpleMapper;
 
     @Autowired
-    public UserResource(UserService userService, UserRoleResource userRoleResource, SimpleMapper mapper) {
+    public UserResource(UserService userService, UserRoleResource userRoleResource, SimpleMapper simpleMapper) {
         this.userService = userService;
         this.userRoleResource = userRoleResource;
-        this.mapper = mapper;
+        this.simpleMapper = simpleMapper;
     }
 
     @GET
     @Path("{userId}")
     public Response getById(@PathParam("userId") long userId, @Context UriInfo uriInfo) {
         try {
-            UserDto userDto = mapper.set(userService.findById(userId)).mapTo(UserDto.class);
+            UserDto userDto = simpleMapper.set(userService.findById(userId)).mapTo(UserDto.class);
             UserResourceLinks userResourceLinks = new UserResourceLinks(uriInfo);
             userDto.getLinks().add(userResourceLinks.getSelf(userId));
             userDto.getLinks().add(userResourceLinks.getRoles(userId));
@@ -48,10 +48,9 @@ public class UserResource {
     }
 
     @GET
-    @Path("all")
     public Response getAll(@Context UriInfo uriInfo) {
         try {
-            List<UserDto> userDtos = mapper.set(userService.findAll()).mapToList(UserDto.class);
+            List<UserDto> userDtos = simpleMapper.set(userService.findAll()).mapToList(UserDto.class);
             userDtos.parallelStream().forEach(userDto -> {
                 UserResourceLinks userResourceLinks = new UserResourceLinks(uriInfo);
                 userDto.getLinks().add(userResourceLinks.getSelf(userDto.getId()));
@@ -67,7 +66,7 @@ public class UserResource {
     @POST
     public Response add(UserDto userDto, @Context UriInfo uriInfo) {
         try {
-            User user = userService.save(mapper.set(userDto).mapTo(User.class));
+            User user = userService.save(simpleMapper.set(userDto).mapTo(User.class));
             UserResourceLinks userResourceLinks = new UserResourceLinks(uriInfo);
             userDto.setId(user.getId());
             userDto.getLinks().add(userResourceLinks.getSelf(userDto.getId()));
@@ -84,9 +83,9 @@ public class UserResource {
     public Response update(@PathParam("userId") Long userId, UserDto userDto, @Context UriInfo uriInfo) {
         try {
             userDto.setId(userId);
-            User user = userService.save(mapper.set(userDto).mapTo(User.class));
+            User user = userService.save(simpleMapper.set(userDto).mapTo(User.class));
             UserResourceLinks userResourceLinks = new UserResourceLinks(uriInfo);
-            userDto = mapper.set(user).mapTo(UserDto.class);
+            userDto = simpleMapper.set(user).mapTo(UserDto.class);
             userDto.getLinks().add(userResourceLinks.getSelf(userDto.getId()));
             userDto.getLinks().add(userResourceLinks.getRoles(userDto.getId()));
             return Response.status(Response.Status.OK).entity(userDto).build();
@@ -100,7 +99,7 @@ public class UserResource {
     @Path("{userId}")
     public Response delete(@PathParam("userId") final Long userId, @Context UriInfo uriInfo) {
         try {
-            UserDto userDto = mapper.set(userService.delete(userId)).mapTo(UserDto.class);
+            UserDto userDto = simpleMapper.set(userService.delete(userId)).mapTo(UserDto.class);
             UserResourceLinks userResourceLinks = new UserResourceLinks(uriInfo);
             userDto.getLinks().add(userResourceLinks.getSelf(userDto.getId()));
             userDto.getLinks().add(userResourceLinks.getRoles(userDto.getId()));
@@ -111,7 +110,7 @@ public class UserResource {
         }
     }
 
-    @Path("{userId}/role")
+    @Path("{userId}/roles")
     public UserRoleResource usersRoleResource(@QueryParam("roleIds") List<Long> roleIds, @Context UriInfo uriInfo) {
         userRoleResource.setRoleIds(roleIds);
         userRoleResource.setUriInfo(uriInfo);
