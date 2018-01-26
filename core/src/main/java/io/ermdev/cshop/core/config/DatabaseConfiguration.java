7b6ad1d7cf65bf45eee199ba982848f5af0c2e41
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-26 18:45:00 +0800

    Improve the autowiring on DatabaseConfiguration

---
+++ core/src/main/java/io/ermdev/cshop/core/config/DatabaseConfiguration.java
@@ -0,0 +1,47 @@
+package io.ermdev.cshop.core.config;
+
+import io.ermdev.cshop.data.repository.RoleRepository;
+import io.ermdev.cshop.data.repository.TokenRepository;
+import io.ermdev.cshop.data.repository.UserRepository;
+import io.ermdev.cshop.data.repository.UserRoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfiguration implements CommandLineRunner {
+
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private UserRoleRepository userRoleRepository;
+    private TokenRepository tokenRepository;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        userRepository.createTable();
+        roleRepository.createTable();
+        userRoleRepository.createTable();
+        tokenRepository.createTable();
+    }
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Autowired
+    public void setRoleRepository(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Autowired
+    public void setUserRoleRepository(UserRoleRepository userRoleRepository) {
+        this.userRoleRepository = userRoleRepository;
+    }
+
+    @Autowired
+    public void setTokenRepository(TokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
+    }
+}
