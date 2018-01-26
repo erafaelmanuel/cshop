a2f52992c2faddc5e5e40657dc29669a3f848a47
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-26 18:59:57 +0800

    Create a TokenDto

---
+++ rest/src/main/java/io/ermdev/cshop/rest/token/TokenDto.java
@@ -0,0 +1,56 @@
+package io.ermdev.cshop.rest.token;
+
+import io.ermdev.cshop.commons.Link;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class TokenDto {
+
+    private Long id;
+    private String key;
+    private Date expiryDate;
+    private Long userId;
+    private List<Link> links = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public List<Link> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<Link> links) {
+        this.links = links;
+    }
+}
