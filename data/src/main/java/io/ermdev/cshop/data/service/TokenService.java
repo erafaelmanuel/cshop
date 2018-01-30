4f10e1305f5688092bcd5d836db45c2c3021e4bb
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-30 14:46:05 +0800

    Update the save method of TokenService

--- data/src/main/java/io/ermdev/cshop/data/service/TokenService.java
+++ data/src/main/java/io/ermdev/cshop/data/service/TokenService.java
@@ -53,4 +53,64 @@ public class TokenService {
             throw new EntityException("No token found");
         }
     }
+
+    public Token save(Token token) throws EntityException {
+        if (token != null) {
+            if (token.getId() == null) {
+                if (token.getKey() == null || token.getKey().trim().isEmpty()) {
+                    throw new EntityException("Key is required");
+                }
+                if (token.getExpiryDate() == null || token.getExpiryDate().toString().trim().isEmpty()) {
+                    throw new EntityException("ExpiryDate is required");
+                }
+                final Long generatedId = IdGenerator.randomUUID();
+                token.setId(generatedId);
+                tokenRepository.add(token);
+                return token;
+            } else {
+                final Token o = tokenRepository.findById(token.getId());
+                if (o != null) {
+                    if (token.getKey() == null && token.getKey().trim().isEmpty()) {
+                        token.setKey(o.getKey());
+                    }
+                    if (token.getExpiryDate() == null && token.getExpiryDate().toString().trim().isEmpty()) {
+                        token.setExpiryDate(o.getExpiryDate());
+                    }
+                    tokenRepository.update(token);
+                    return token;
+                } else {
+                    token.setId(null);
+                    return save(token);
+                }
+            }
+        } else {
+            return null;
+        }
+    }
+
+//    public Token delete(Long tokenId) throws EntityException {
+//        final TokenDto tokenDto = tokenRepository.findById(tokenId);
+//        if (tokenDto != null) {
+//            tokenRepository.delete(tokenDto);
+//            return modelMapper.set(tokenDto)
+//                    .field("userId", "user")
+//                    .convertFieldToType("user", User.class)
+//                    .getTransaction().mapTo(Token.class);
+//        } else {
+//            throw new EntityException("No token found");
+//        }
+//    }
+//
+//    public Token delete(Token token) throws EntityException {
+//        final TokenDto tokenDto = tokenRepository.findById(token.getId());
+//        if (tokenDto != null) {
+//            tokenRepository.delete(tokenDto);
+//            return modelMapper.set(tokenDto)
+//                    .field("userId", "user")
+//                    .convertFieldToType("user", User.class)
+//                    .getTransaction().mapTo(Token.class);
+//        } else {
+//            throw new EntityException("No token found");
+//        }
+//    }
 }
