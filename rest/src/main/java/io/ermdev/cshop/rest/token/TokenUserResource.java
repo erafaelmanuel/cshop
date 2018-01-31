cfaeb93ee2176a13ed15c1655b285998ceae0987
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-31 17:01:53 +0800

    Update the TokenUserResource

--- rest/src/main/java/io/ermdev/cshop/rest/token/TokenUserResource.java
+++ rest/src/main/java/io/ermdev/cshop/rest/token/TokenUserResource.java
@@ -1,15 +1,41 @@
 package io.ermdev.cshop.rest.token;
 
+import io.ermdev.cshop.commons.Error;
+import io.ermdev.cshop.data.entity.User;
+import io.ermdev.cshop.data.repository.TokenUserRepository;
+import io.ermdev.cshop.data.service.TokenUserService;
+import io.ermdev.cshop.exception.EntityException;
+import io.ermdev.cshop.rest.user.UserDto;
+import io.ermdev.cshop.rest.user.UserResourceLinks;
+import io.ermdev.mapfierj.SimpleMapper;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Component
 @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-@Path("users")
+@Path("user")
 public class TokenUserResource {
+
+    private TokenUserService tokenUserService;
+    private SimpleMapper simpleMapper;
+
+    public TokenUserResource(TokenUserService tokenUserService, SimpleMapper simpleMapper) {
+        this.tokenUserService = tokenUserService;
+        this.simpleMapper = simpleMapper;
+    }
+
+    @GET
+    public Response findById(@PathParam("tokenId") Long tokenId) {
+        try {
+            UserDto userDto = simpleMapper.set(tokenUserService.findUserByTokenId(tokenId)).mapTo(UserDto.class);
+            return Response.status(Response.Status.FOUND).entity(userDto).build();
+        } catch (EntityException e) {
+            Error error = new Error(e.getMessage());
+            return Response.status(Response.Status.NOT_FOUND).entity(error).build();
+        }
+    }
 }
