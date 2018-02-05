59f7fa3466ffb877b88c4df933ab715828fc58e1
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-05 20:33:19 +0800

    Create an Image class

---
+++ data/src/main/java/io/ermdev/cshop/data/entity/Image.java
@@ -0,0 +1,23 @@
+package io.ermdev.cshop.data.entity;
+
+public class Image {
+
+    private Long id;
+    private String src;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSrc() {
+        return src;
+    }
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
+}
