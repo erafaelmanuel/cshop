eb525956f860f8986d948a2a5bdabb35f5e43820
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-06 23:06:12 +0800

    Update the ImageResource

--- rest/src/main/java/io/ermdev/cshop/rest/image/ImageResource.java
+++ rest/src/main/java/io/ermdev/cshop/rest/image/ImageResource.java
@@ -1,15 +1,44 @@
 package io.ermdev.cshop.rest.image;
 
+import io.ermdev.cshop.commons.Error;
+import io.ermdev.cshop.data.service.ImageService;
+import io.ermdev.cshop.exception.EntityException;
+import io.ermdev.mapfierj.SimpleMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 @Component
 @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 @Path("images")
 public class ImageResource {
+
+    private ImageService imageService;
+    private SimpleMapper simpleMapper;
+
+    @Autowired
+    public ImageResource(ImageService imageService, SimpleMapper simpleMapper) {
+        this.imageService = imageService;
+        this.simpleMapper = simpleMapper;
+    }
+
+    @GET
+    @Path("{imageId}")
+    public Response getById(@PathParam("imageId") long imageId, @Context UriInfo uriInfo) {
+        try {
+            ImageDto imageDto = simpleMapper.set(imageService.findById(imageId)).mapTo(ImageDto.class);
+            ImageResourceLinks imageResourceLinks = new ImageResourceLinks(uriInfo);
+            imageDto.getLinks().add(imageResourceLinks.getSelf(imageId));
+            return Response.status(Response.Status.OK).entity(imageDto).build();
+        } catch (EntityException e) {
+            Error error = new Error(e.getMessage());
+            return Response.status(Response.Status.NOT_FOUND).entity(error).build();
+        }
+    }
 }
