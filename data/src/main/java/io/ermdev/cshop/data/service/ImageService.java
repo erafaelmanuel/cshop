c2e0666ef0ade517bd47ada17794b7afb5a749ee
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-07 20:38:02 +0800

    Create a method to save an image

--- data/src/main/java/io/ermdev/cshop/data/service/ImageService.java
+++ data/src/main/java/io/ermdev/cshop/data/service/ImageService.java
@@ -1,5 +1,6 @@
 package io.ermdev.cshop.data.service;
 
+import io.ermdev.cshop.commons.IdGenerator;
 import io.ermdev.cshop.data.entity.Image;
 import io.ermdev.cshop.data.repository.ImageItemRepository;
 import io.ermdev.cshop.data.repository.ImageRepository;
@@ -45,6 +46,34 @@ public class ImageService {
             return images;
         } else {
             throw new EntityException("No image found");
+        }
+    }
+
+    public Image save(Image image) throws EntityException {
+        if(image != null) {
+            if(image.getId() == null) {
+                if(image.getSrc() == null || image.getSrc().trim().isEmpty()) {
+                    throw new EntityException("Src is required");
+                }
+                final Long generatedId = IdGenerator.randomUUID();
+                image.setId(generatedId);
+                imageRepository.add(image);
+                return image;
+            } else {
+                final Image o = imageRepository.findById(image.getId());
+                if(o != null) {
+                    if(image.getSrc() == null || image.getSrc().trim().isEmpty()) {
+                        image.setSrc(o.getSrc());
+                    }
+                    imageRepository.update(image);
+                    return image;
+                } else {
+                    image.setId(null);
+                    return save(image);
+                }
+            }
+        } else {
+            throw new NullPointerException("Image is null");
         }
     }
 }
