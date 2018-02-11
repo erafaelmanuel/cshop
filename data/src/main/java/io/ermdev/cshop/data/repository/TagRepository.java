653981dc4aeac4df2dbc2390ac20223173157b0b
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-11 20:35:00 +0800

    Update TagRepository

--- data/src/main/java/io/ermdev/cshop/data/repository/TagRepository.java
+++ data/src/main/java/io/ermdev/cshop/data/repository/TagRepository.java
@@ -1,41 +1,12 @@
 package io.ermdev.cshop.data.repository;
 
-import io.ermdev.cshop.data.entity.Tag;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 
-import java.util.List;
-
-@Deprecated
 @Mapper
 public interface TagRepository {
 
-    @Select("select * from tbltag where id = #{tagId}")
-    Tag findById(@Param("tagId") Long tagId);
-
-    @Select("select _T.id as id, _T.title as title, _T.description as description, _T.keyword as keyword from " +
-            "tblitem_tag as _IT join tbltag as _T on _IT.tagId=_T.id where _IT.itemId=#{itemId} group by " +
-            "_IT.tagId")
-    List<Tag> findByItemId(@Param("itemId") Long itemId);
-
-    @Select("select * from tbltag")
-    List<Tag> findAll();
-
-    @Select("select _T.id as id, _T.title as title, _T.description as description, _T.keyword as keyword from " +
-            "tblrelated_tag as _RT join tbltag as _T on _RT.relatedTagId=_T.id WHERE _RT.tagId=#{tagId}")
-    List<Tag> findRelatedTag(@Param("tagId") Long tagId);
-
-    @Insert("insert into tbltag(id, title, description, keyword) values(#{id}, #{title}, #{description}, #{keyword})")
-    void add(Tag tag);
-
-    @Insert("insert into tblrelated_tag(tagId, relatedTagId) values(#{tagId}, #{relatedTagId})")
-    void addRelatedTag(@Param("tagId") Long tagId, @Param("relatedTagId") Long relatedTagId);
-
-    @Update("update tbltag set title=#{title}, description=#{description}, keyword=#{keyword} where id=#{id}")
-    void updateById(Tag tag);
-
-    @Delete("delete from tbltag where id=#{tagId}")
-    void deleteById(Long tagId);
-
-    @Delete("delete from tblrelated_tag where tagId=#{tagId} and relatedTagId=#{relatedTagId}")
-    void deleteRelatedTag(@Param("tagId") Long tagId, @Param("relatedTagId") Long relatedTagId);
+    @Insert("CREATE TABLE IF NOT EXISTS tbl_tag(id BIGINT NOT NULL AUTO_INCREMENT, name VARCHAR(100), description " +
+            "VARCHAR(200), PRIMARY KEY(id))")
+    void createTable();
 }
