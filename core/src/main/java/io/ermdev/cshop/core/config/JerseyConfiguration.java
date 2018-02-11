2bb59e3bcc352bd2dcf6dee6e7493c5ecae68236
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-11 21:31:14 +0800

    Register TagResource

--- core/src/main/java/io/ermdev/cshop/core/config/JerseyConfiguration.java
+++ core/src/main/java/io/ermdev/cshop/core/config/JerseyConfiguration.java
@@ -1,9 +1,9 @@
 package io.ermdev.cshop.core.config;
 
-import io.ermdev.cshop.rest.category.CategoryResource;
 import io.ermdev.cshop.rest.image.ImageResource;
 import io.ermdev.cshop.rest.item.ItemResource;
 import io.ermdev.cshop.rest.role.RoleResource;
+import io.ermdev.cshop.rest.tag.TagResource;
 import io.ermdev.cshop.rest.token.TokenResource;
 import io.ermdev.cshop.rest.user.UserResource;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -21,7 +21,6 @@ public class JerseyConfiguration extends ResourceConfig {
         register(TokenResource.class);
         register(ItemResource.class);
         register(ImageResource.class);
-        register(CategoryResource.class);
         register(TagResource.class);
     }
 }
