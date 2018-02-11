896bb5d45dd95fd729736b40fcb6ed407d0f1c98
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-11 21:28:43 +0800

    Update TagResource

--- rest/src/main/java/io/ermdev/cshop/rest/tag/TagResource.java
+++ rest/src/main/java/io/ermdev/cshop/rest/tag/TagResource.java
@@ -1,9 +1,9 @@
 package io.ermdev.cshop.rest.tag;
 
 import io.ermdev.cshop.commons.Error;
+import io.ermdev.cshop.data.entity.Tag;
 import io.ermdev.cshop.data.service.TagService;
 import io.ermdev.cshop.exception.EntityException;
-import io.ermdev.cshop.rest.token.TokenDto;
 import mapfierj.SimpleMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -18,7 +18,7 @@ import java.util.List;
 @Component
 @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-@Path("tokens")
+@Path("tags")
 public class TagResource {
 
     private TagService tagService;
@@ -31,12 +31,12 @@ public class TagResource {
     }
 
     @GET
-    @Path("{tokenId}")
-    public Response getById(@PathParam("tokenId") Long tokenId, @Context UriInfo uriInfo) {
+    @Path("{tagId}")
+    public Response getById(@PathParam("tagId") Long tagId, @Context UriInfo uriInfo) {
         try {
-            TagDto tagDto = simpleMapper.set(tagService.findById(tokenId)).mapTo(tagDto.class);
+            TagDto tagDto = simpleMapper.set(tagService.findById(tagId)).mapTo(TagDto.class);
             TagResourceLinks tagResourceLinks = new TagResourceLinks(uriInfo);
-            tagDto.getLinks().add(tagResourceLinks.getSelf(tokenId));
+            tagDto.getLinks().add(tagResourceLinks.getSelf(tagId));
             return Response.status(Response.Status.FOUND).entity(tagDto).build();
         } catch (EntityException e) {
             Error error = new Error(e.getMessage());
@@ -47,13 +47,57 @@ public class TagResource {
     @GET
     public Response getAll(@Context UriInfo uriInfo) {
         try {
-            List<TagDto> tagDtos = simpleMapper.set(tokenService.findAll()).mapToList(TokenDto.class);
+            List<TagDto> tagDtos = simpleMapper.set(tagService.findAll()).mapToList(TagDto.class);
             tagDtos.parallelStream().forEach(tagDto -> {
                 TagResourceLinks tagResourceLinks = new TagResourceLinks(uriInfo);
                 tagDto.getLinks().add(tagResourceLinks.getSelf(tagDto.getId()));
             });
             return Response.status(Response.Status.FOUND).entity(tagDtos).build();
         } catch (EntityException e) {
+            Error error = new Error(e.getMessage());
+            return Response.status(Response.Status.NOT_FOUND).entity(error).build();
+        }
+    }
+
+    @POST
+    public Response add(TagDto tagDto, @Context UriInfo uriInfo) {
+        try {
+            Tag tag = tagService.save(simpleMapper.set(tagDto).mapTo(Tag.class));
+            TagResourceLinks tagResourceLinks = new TagResourceLinks(uriInfo);
+            tagDto.setId(tag.getId());
+            tagDto.getLinks().add(tagResourceLinks.getSelf(tagDto.getId()));
+            return Response.status(Response.Status.OK).entity(tagDto).build();
+        } catch (EntityException e) {
+            Error error = new Error(e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(error).build();
+        }
+    }
+
+    @PUT
+    @Path("{tagId}")
+    public Response update(@PathParam("tagId") Long tagId, TagDto tagDto, @Context UriInfo uriInfo) {
+        try {
+            tagDto.setId(tagId);
+            Tag tag = tagService.save(simpleMapper.set(tagDto).mapTo(Tag.class));
+            TagResourceLinks tagResourceLinks = new TagResourceLinks(uriInfo);
+            tagDto = simpleMapper.set(tag).mapTo(TagDto.class);
+            tagDto.getLinks().add(tagResourceLinks.getSelf(tagDto.getId()));
+            return Response.status(Response.Status.OK).entity(tagDto).build();
+        } catch (EntityException e) {
+            Error error = new Error(e.getMessage());
+            return Response.status(Response.Status.NOT_FOUND).entity(error).build();
+        }
+    }
+
+    @DELETE
+    @Path("{tagId}")
+    public Response delete(@PathParam("tagId") final Long tagId, @Context UriInfo uriInfo) {
+        try {
+            TagDto tagDto = simpleMapper.set(tagService.delete(tagId)).mapTo(TagDto.class);
+            TagResourceLinks tagResourceLinks = new TagResourceLinks(uriInfo);
+            tagDto.getLinks().add(tagResourceLinks.getSelf(tagDto.getId()));
+            return Response.status(Response.Status.OK).entity(tagDto).build();
+        } catch (Exception e) {
             Error error = new Error(e.getMessage());
             return Response.status(Response.Status.NOT_FOUND).entity(error).build();
         }
