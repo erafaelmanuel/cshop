81757b2e06c8869e37c4508c81655676f0d918bc
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-12 11:35:01 +0800

    Delete EmailExistsException

--- data/src/main/java/io/ermdev/cshop/data/exception/EmailExistsException.java
+++
@@ -1,8 +0,0 @@
-package io.ermdev.cshop.data.exception;
-
-public class EmailExistsException extends Exception {
-
-    public EmailExistsException(final String message) {
-        super(message);
-    }
-}
