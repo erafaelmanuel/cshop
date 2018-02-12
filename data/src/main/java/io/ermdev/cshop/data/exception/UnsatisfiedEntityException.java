292ea0c80613a79e80ac03d924f2aa80770f97fc
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-12 11:41:01 +0800

    Delete UnsatisfiedEntityException

--- data/src/main/java/io/ermdev/cshop/data/exception/UnsatisfiedEntityException.java
+++
@@ -1,8 +0,0 @@
-package io.ermdev.cshop.data.exception;
-
-public class UnsatisfiedEntityException extends Exception {
-
-    public UnsatisfiedEntityException(String message) {
-        super(message);
-    }
-}
