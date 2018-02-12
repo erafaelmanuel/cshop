d35463afe7d09babb63a5b3bbc0d31dcd9596c3b
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-12 11:07:23 +0800

    Update Attribute class

--- data/src/main/java/io/ermdev/cshop/data/entity/Attribute.java
+++ data/src/main/java/io/ermdev/cshop/data/entity/Attribute.java
@@ -1,31 +1,10 @@
 package io.ermdev.cshop.data.entity;
 
-import io.ermdev.cshop.commons.Link;
-
-import javax.xml.bind.annotation.XmlRootElement;
-import java.util.ArrayList;
-import java.util.List;
-
-@Deprecated
-@XmlRootElement
 public class Attribute {
 
     private Long id;
-
-    private String title;
-    private String content;
-    private String description;
+    private String name;
     private String type;
-    private List<Link> links = new ArrayList<>();
-
-    public Attribute() {}
-
-    public Attribute(String title, String content, String description, String type) {
-        this.title = title;
-        this.content = content;
-        this.description = description;
-        this.type = type;
-    }
 
     public Long getId() {
         return id;
@@ -35,28 +14,12 @@ public class Attribute {
         this.id = id;
     }
 
-    public String getTitle() {
-        return title;
+    public String getName() {
+        return name;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getType() {
@@ -65,24 +28,5 @@ public class Attribute {
 
     public void setType(String type) {
         this.type = type;
-    }
-
-    public List<Link> getLinks() {
-        return links;
-    }
-
-    public void setLinks(List<Link> links) {
-        this.links = links;
-    }
-
-    @Override
-    public String toString() {
-        return "Attribute{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                ", description='" + description + '\'' +
-                ", type='" + type + '\'' +
-                '}';
     }
 }
