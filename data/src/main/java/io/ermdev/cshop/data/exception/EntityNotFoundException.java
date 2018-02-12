181a0fbcb2c679004b12e0261bfb618a57d0a093
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-12 11:36:13 +0800

    Delete

--- data/src/main/java/io/ermdev/cshop/data/exception/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package io.ermdev.cshop.data.exception;
-
-public class EntityNotFoundException extends Exception {
-
-    public EntityNotFoundException(final String message) {
-        super(message);
-    }
-}
