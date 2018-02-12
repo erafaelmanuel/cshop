e785ca870a93db7f6a210213073796964dae548c
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-12 10:44:47 +0800

    Update CategoryRepository class

--- data/src/main/java/io/ermdev/cshop/data/repository/CategoryRepository.java
+++ data/src/main/java/io/ermdev/cshop/data/repository/CategoryRepository.java
@@ -5,30 +5,30 @@ import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
-@Deprecated
 @Mapper
 public interface CategoryRepository {
 
-    @Select("select * from tblcategory where id = #{categoryId}")
+    @Insert("CREATE TABLE IF NOT EXISTS tbl_category(id BIGINT NOT NULL AUTO_INCREMENT, name VARCHAR(100), " +
+            "description VARCHAR(200), parentId BIGINT DEFAULT NULL, PRIMARY KEY(id), FOREIGN KEY(parentId) " +
+            "REFERENCES tbl_category(id) ON DELETE CASCADE ON UPDATE CASCADE)")
+    void createTable();
+
+    @Select("SELECT * FROM tbl_category WHERE id=#{categoryId}")
     Category findById(@Param("categoryId") Long categoryId);
 
-    @Select("select _C.id as id, _C.name as name, _C.description as description, _C.parentId as parentId from " +
-            "tblitem as _I left join tblcategory as _C on _I.categoryId=_C.id where _I.id = #{itemId}")
-    Category findByItemId(@Param("itemId") Long itemId);
+    @Select("SELECT * FROM tbl_category WHERE parentId=#{parentId}")
+    List<Category> findByParentId(@Param("parentId") Long parentId);
 
-    @Select("select * from tblcategory where parentId=#{parentId}")
-    List<Category> findByParent(@Param("parentId") Long parentId);
-
-    @Select("select * from tblcategory")
+    @Select("SELECT * FROM tbl_category")
     List<Category> findAll();
 
-    @Insert("insert into tblcategory(id, name, description, parentId) values(#{id}, #{name}, #{description}, " +
+    @Insert("INSERT INTO tbl_category(id, name, description, parentId) VALUES(#{id}, #{name}, #{description}, " +
             "#{parentId})")
     void add(Category category);
 
-    @Update("update tblcategory set name=#{name}, description=#{description}, parentId=#{parentId} where id=#{id}")
-    void updateById(Category category);
+    @Update("UPDATE tbl_category SET name=#{name}, description=#{description}, parentId=#{parentId} WHERE id=#{id}")
+    void update(Category category);
 
-    @Delete("delete from tblcategory where id=#{categoryId}")
-    void deleteById(Long categoryId);
+    @Delete("DELETE FROM tbl_category WHERE id=#{categoryId}")
+    void delete(Category category);
 }
