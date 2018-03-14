fd4bb2f9760c9e60e3c587924bcc4047ddf2721c
erafaelmanuel <erafaelmanuel@gmail.com>
2018-03-14 23:30:59 +0800

    Create a ResourceSupport class

---
+++ commons/src/main/java/io/ermdev/cshop/commons/ResourceSupport.java
@@ -0,0 +1,17 @@
+package io.ermdev.cshop.commons;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResourceSupport {
+
+    private List<Link> links = new ArrayList<>();
+
+    public List<Link> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<Link> links) {
+        this.links = links;
+    }
+}
