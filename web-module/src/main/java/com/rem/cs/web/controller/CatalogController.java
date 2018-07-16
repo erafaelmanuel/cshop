f61823692810baa9f7fd8156f802a44ce6c5dc59
Rafael Manuel <erafaelmanuel@gmail.com>
2018-07-16 23:59:33 +0800

    Create a simple pagination for the catalog

--- web-module/src/main/java/com/rem/cs/web/controller/CatalogController.java
+++ web-module/src/main/java/com/rem/cs/web/controller/CatalogController.java
@@ -1,9 +1,11 @@
 package com.rem.cs.web.controller;
 
+import com.rem.cs.data.jpa.item.Item;
 import com.rem.cs.data.jpa.item.ItemService;
 import com.rem.cs.web.dto.ItemDto;
 import com.rem.mappyfy.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
@@ -26,17 +28,63 @@ public class CatalogController {
     }
 
     @GetMapping("/catalog")
-    public String getCatalog(@PageableDefault(sort = "name") Pageable pageable, Model model) {
+    public String getCatalog(@PageableDefault(sort = "name", size = 1) Pageable pageable, Model model) {
         try {
-            final List<ItemDto> items = new ArrayList<>();
             final Mapper mapper = new Mapper();
+            final List<ItemDto> items = new ArrayList<>();
+            final Page<Item> pageItems = itemService.findAll(pageable);
+            final int pages[] = new int[pageItems.getTotalPages() < 5 ? pageItems.getTotalPages() : 5];
+            int currentPage = pageable.getPageNumber() + 1;
+            boolean prevEllipsis = false;
+            boolean nextEllipsis = false;
 
-            itemService.findAll(pageable).forEach(item -> items.add(
+
+            pageItems.forEach(item -> items.add(
                     mapper
                             .set(item)
                             .ignore("categories")
                             .mapTo(ItemDto.class)));
+
+            if (currentPage > pageItems.getTotalPages()) {
+                currentPage = 1;
+            }
+            if (pages.length == 5) {
+                if (currentPage < pages.length-1) {
+                    for (int i = 0; i < pages.length; i++) {
+                        pages[i] = i + 1;
+                    }
+                    if (pageItems.getTotalPages() > pages.length) {
+                        nextEllipsis = true;
+                    }
+                } else {
+                    final int start = currentPage - 2;
+
+                    if (currentPage + 2 >= pageItems.getTotalPages()) {
+                        for (int i = 0; i < pages.length; i++) {
+                            pages[(pages.length-1) - i] = pageItems.getTotalPages() - i;
+                        }
+                        prevEllipsis = true;
+                    } else {
+                        for (int i = 0; i < pages.length; i++) {
+                            pages[i] = start + i;
+                        }
+                        prevEllipsis = true;
+                        nextEllipsis = true;
+                    }
+                }
+            } else {
+                for (int i = 0; i < pages.length; i++) {
+                    pages[i] = i + 1;
+                }
+            }
+
             model.addAttribute("items", items);
+            model.addAttribute("pages", pages);
+            model.addAttribute("cp", currentPage);
+            model.addAttribute("prevEllipsis", prevEllipsis);
+            model.addAttribute("nextEllipsis", nextEllipsis);
+            model.addAttribute("isFirst", pageItems.isFirst());
+            model.addAttribute("isLast", pageItems.isLast());
         } catch (Exception e) {
             e.printStackTrace();
         }
