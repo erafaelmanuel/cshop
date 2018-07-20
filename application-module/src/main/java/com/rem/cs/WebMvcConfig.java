840004ae241d43fb9af13b8d72ab2930fe87874a
Rafael Manuel <erafaelmanuel@gmail.com>
2018-07-20 14:43:51 +0800

    Update web-module

--- application-module/src/main/java/com/rem/cs/WebMvcConfig.java
+++ application-module/src/main/java/com/rem/cs/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.rem.cs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         final String[] resourceLocation = {"file:images/", "file:c:/images/"};
         registry.addResourceHandler("/images/**").addResourceLocations(resourceLocation);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/validate").setViewName("register/validating");
     }
 }
