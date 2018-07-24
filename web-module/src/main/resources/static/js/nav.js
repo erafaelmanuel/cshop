cb6896873006651db43b482893eeda676037f220
Rafael Manuel <erafaelmanuel@gmail.com>
2018-07-24 16:05:49 +0800

    Update navigation

--- web-module/src/main/resources/static/js/nav.js
+++ web-module/src/main/resources/static/js/nav.js
@@ -1,37 +1,42 @@
 (function(){
     var xmlhttp = new XMLHttpRequest();
 
-        $(document).on("mouseover", ".category-1 .list-item", function() {
-            var params = "cid=" + $(this).attr("categoryId");
+        $(document).on("mouseenter", ".list-item", function() {
+            $item = $(this);
+            var params = "cid=" + $item.attr("categoryId");
             var url = "/category/subCategoriesOf?" + params;
+
             xmlhttp.onreadystatechange = function() {
-                $(".category-2").empty();
-                $(".category-2").css("display", "none");
-                $(".category-2").css("height", $(".category-1").css("height"));
                 if(xmlhttp.status == 200 && xmlhttp.readyState == 4) {
                     if ($(xmlhttp.responseText).find(".list-item").length > 0) {
-                        $(".category-2").css("display", "block");
-                        $(".category-2").append($(xmlhttp.responseText).find(".list").html());
-                    } else {
-                        $(".category-2").css("display", "none");
+                        $item.addClass("item-collapse");
+                        if ( $item.find(".category-child").length > 0) {
+                            $item.find(".category-child").empty();
+                        } else {
+                            $item.append($(document.createElement('ul'))
+                                .css("height", $(".category-parent").css("height"))
+                                .addClass("list category-child")
+                            );
+                        }
+                        $item.find(".category-child").append($(xmlhttp.responseText).find(".list-item"));
                     }
-                    console.log($(xmlhttp.responseText).find(".list").html());
-                    console.log(xmlhttp.responseText);
                 }
             }
             xmlhttp.open("POST", url, true);
             xmlhttp.send();
         });
 
+        $(document).on("mouseleave", ".list-item", function() {
+            $(this).removeClass("item-collapse");
+            $(this).find(".list").remove();
+        });
+
+        $(document).on("mouseenter", ".categories", function() {
+            $(".category-parent").css("display", "block");
+        });
+
         $(document).on("mouseleave", ".categories", function() {
-            $(".category-2").empty();
-            $(".category-2").css("display", "none");
-            $(".category-1").css("display", "none");
+            $(".category-parent").css("display", "none");
         });
-
-        $(document).on("mouseover", ".categories", function() {
-            $(".category-1").css("display", "block");
-        });
-
 
 })();
